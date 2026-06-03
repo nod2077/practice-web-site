a3a13a9678394a6d8fe1046a63e3283299fd0a01
NOD27 <101178286+nod2077@users.noreply.github.com>
2026-06-04 01:16:12 +0500

    Delete Сайт directory

--- Сайт/prompt.js
+++
@@ -1,4 +0,0 @@
-let name = prompt('Как вас зовут?');
-let age = prompt('Сколько вам лет?');
-
-alert(`Привет, ${name}! Тебе ${age} лет.`);
--- Сайт/script.js
+++
@@ -1,4 +0,0 @@
-function toggleVisibility() {
-    const element = document.getElementById('myDiv');
-    element.classList.toggle('hidden');
-}
